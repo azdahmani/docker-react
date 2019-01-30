63eedf961929fe4ae2eb3d09a91ffbad89d5fda6
adahmani <abdel.dahmani@artemis-ie.com>
2019-01-30 13:01:15 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React with Abdelaziz Dahmani 2019.
           </a>
         </header>
       </div>
